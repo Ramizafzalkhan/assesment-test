b396215e32e7198dbf4823d1b6823f9c17961b95
ramiz <rameez@ccjk.com>
2021-10-13 12:33:53 +0500

    completed flow

--- src/App/components/features/feature.js
+++ src/App/components/features/feature.js
@@ -3,13 +3,13 @@ import FeatureCard from './featureCard';
 
 function Feature() {
     const item = [
-        { id: 1, name: "Swipe Swap", des: "Discover new and unique NFTs with our Tinder-style swipe and match feature, Swipe Swap, which will recommend you NFTs based on your interests." },
-        { id: 2, name: "Aggregated Marketplace", des: "Buy and sell NFTs on our Aggregated Marketplace – our dedicated marketplace listing all NFTs for sale from various popular NFT marketplaces. Boost your impressions by utilizing our self-promotion, features." },
-        { id: 3, name: "Art Lottery", des: "Get an ART Lottery ticket with every swap or purchase tickets directly and you could be one of our weekly Lottery winners, receiving a pot of $ART tokens and a rare NFT. " },
-        { id: 4, name: "Social Platform", des: "Engage with other users and their content – follow their accounts, like, comment, and bid on their NFTs. Create NFT playlists, forums, and more." },
-        { id: 5, name: "Swap Auctions", des: "Play our Art Auctions – countdown-style bids that award the NFT to the highest bidder. Compete for the winning bid in Party Mode, or use Silent-Auction Mode to discretely place your offers. Auctioneers receive bonus ART Lottery tickets upon sale!" },
-        { id: 6, name: "MyArt", des: "Create your personal profile where you can define your interests, and display your NFTs in a personalized way. Choose your avatar and how you want to showcase your collectibles. Add friends, and connect to Buyers/Sellers in a new way via direct message." },
-        { id: 7, name: "NFT Vault", des: "Safely and securely store your precious NFTs in your personal NFT Vault. Accessible by password on browser version or using Face ID on iPhone, Swap Vault ensures an extra layer of security with 2FA and platform insurance protocols." },
+        { id: 1, icon:"assets/images/icon.svg", name: "Swipe Swap", des: "Discover new and unique NFTs with our Tinder-style swipe and match feature, Swipe Swap, which will recommend you NFTs based on your interests." },
+        { id: 2, icon:"assets/images/icon-2.svg", name: "Aggregated Marketplace", des: "Buy and sell NFTs on our Aggregated Marketplace – our dedicated marketplace listing all NFTs for sale from various popular NFT marketplaces. Boost your impressions by utilizing our self-promotion, features." },
+        { id: 3, icon:"assets/images/icon-3.svg", name: "Art Lottery", des: "Get an ART Lottery ticket with every swap or purchase tickets directly and you could be one of our weekly Lottery winners, receiving a pot of $ART tokens and a rare NFT. " },
+        { id: 4, icon:"assets/images/icon-4.svg", name: "Social Platform", des: "Engage with other users and their content – follow their accounts, like, comment, and bid on their NFTs. Create NFT playlists, forums, and more." },
+        { id: 5, icon:"assets/images/icon-5.svg", name: "Swap Auctions", des: "Play our Art Auctions – countdown-style bids that award the NFT to the highest bidder. Compete for the winning bid in Party Mode, or use Silent-Auction Mode to discretely place your offers. Auctioneers receive bonus ART Lottery tickets upon sale!" },
+        { id: 6, icon:"assets/images/icon-6.svg", name: "MyArt", des: "Create your personal profile where you can define your interests, and display your NFTs in a personalized way. Choose your avatar and how you want to showcase your collectibles. Add friends, and connect to Buyers/Sellers in a new way via direct message." },
+        { id: 7, icon:"assets/images/icon-7.svg", name: "NFT Vault", des: "Safely and securely store your precious NFTs in your personal NFT Vault. Accessible by password on browser version or using Face ID on iPhone, Swap Vault ensures an extra layer of security with 2FA and platform insurance protocols." },
     ];
     return (
         <div className="feature-section">
--- src/App/components/features/featureCard.js
+++ src/App/components/features/featureCard.js
@@ -4,7 +4,7 @@ function FeatureCard(props) {
     return (
         <div className="bg-theme rounded-xl  p-10">
             <div className="bg-white relative w-10 h-10 rounded-full">
-            <img className="absolute top-0" src="assets/images/icon.svg" alt="" />
+            <img className="absolute top-0 right-0" src={props.item.icon} alt="" style={{width:26, height:40}} />
             </div>
             <h2 className="text-xl font-bold pb-2.5">{props.item.name}</h2>
             <p>{props.item.des}</p>
